121d99fe88b68794a8a7205de62112ab0e9ad83f
Guilherme <guilherme-thums@Hotmail.com>
2019-09-22 21:21:59 -0300

    Exercicio 16

--- app/src/main/java/com/example/exe16/MainActivity.java
+++ app/src/main/java/com/example/exe16/MainActivity.java
@@ -61,15 +61,15 @@ public class MainActivity extends AppCompatActivity {
                 // find which radio button is selected
                 if (checkedId == R.id.ads) {
                     adsEscolhido = true;
-                    Toast.makeText(getApplicationContext(), "Escolhido: Ads",
+                    Toast.makeText(getApplicationContext(), "Escolhido: Análise e Desenvolvimento de Sistemas",
                             Toast.LENGTH_SHORT).show();
-                } else if (checkedId == R.id.eng) {
+                } else if (checkedId == R.id.engenhariaComp) {
                     engEscolhido = true;
-                    Toast.makeText(getApplicationContext(), "Escolhido: Eng",
+                    Toast.makeText(getApplicationContext(), "Escolhido: Engenharia da Computação",
                             Toast.LENGTH_SHORT).show();
-                } else if (checkedId == R.id.cie) {
+                } else if (checkedId == R.id.cienciaComp) {
                     cieEscolhido = true;
-                    Toast.makeText(getApplicationContext(), "Escolhido: Ciências",
+                    Toast.makeText(getApplicationContext(), "Escolhido: Ciências da computacão",
                             Toast.LENGTH_SHORT).show();
                 }
             }
@@ -84,10 +84,10 @@ public class MainActivity extends AppCompatActivity {
                 // find which radio button is selected
                 // find which radio button is selected
                 if (checkedId == R.id.notiSim) {
-                    Toast.makeText(getApplicationContext(), "Escolhido: SIM",
+                    Toast.makeText(getApplicationContext(), "Escolhido: Sim",
                             Toast.LENGTH_SHORT).show();
-                } else if (checkedId == R.id.eng) {
-                    Toast.makeText(getApplicationContext(), "Escolhido: NAO",
+                } else if (checkedId == R.id.notiNao) {
+                    Toast.makeText(getApplicationContext(), "Escolhido: Não",
                             Toast.LENGTH_SHORT).show();
                 }
             }
@@ -97,29 +97,29 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view) {
         final Context contexto = getApplicationContext();
         String texto;
-        final int duracao = Toast.LENGTH_LONG;
+        final int duracao = Toast.LENGTH_SHORT;
 
-        EditText matricula = findViewById(R.id.Matricula);
+        EditText matricula = findViewById(R.id.matricula);
         String matriculaText = matricula.getText().toString();
 
-        EditText nome = findViewById(R.id.Nome);
+        EditText nome = findViewById(R.id.nome);
         String nomeText = nome.getText().toString();
 
-        EditText email = findViewById(R.id.Email);
+        EditText email = findViewById(R.id.email);
         String emailText = email.getText().toString();
 
-        EditText senha = findViewById(R.id.Senha);
+        EditText senha = findViewById(R.id.senha);
         String senhaText = senha.getText().toString();
 
-        EditText confSenha = findViewById(R.id.confSenha);
+        EditText confSenha = findViewById(R.id.contraSenha);
         String confSenhaText = confSenha.getText().toString();
 
         EditText date = findViewById(R.id.Date);
         String dateText = date.getText().toString();
 
-        CheckBox checkCie = findViewById(R.id.cieCheck);
-        CheckBox checkAds = findViewById(R.id.checkAds);
-        CheckBox checkEng = findViewById(R.id.EngCheck);
+        CheckBox checkCiencias = findViewById(R.id.cienciaCompCheck);
+        CheckBox checkAds = findViewById(R.id.AdsCheck);
+        CheckBox checkEngenharia = findViewById(R.id.engenhariaCompCheck);
 
         boolean letrasRegex = Pattern.matches("\\d", nomeText);
         boolean numerosRegex = Pattern.matches("\\w", matriculaText);
@@ -135,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
                 Date strDate = format.parse(dateText);
                 Date atual = new Date();
                 if (strDate.after(atual)) {
-                    Toast toast = Toast.makeText(contexto, "data invalida", duracao);
+                    Toast toast = Toast.makeText(contexto, "data inválida", duracao);
                     toast.show();
                 } else {
                     Toast toast = Toast.makeText(contexto, "data valida", duracao);
@@ -147,56 +147,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-//        if (TextUtils.isEmpty(matriculaText)) {
-//            Toast toast = Toast.makeText(contexto, "A matricula nao pode ser vazio", duracao);
-//            toast.show();
-//        } else if (matriculaText.length() > 20) {
-//            Toast toast = Toast.makeText(contexto, "Insira uma matricula de tamanho valido", duracao);
-//            toast.show();
-//        } else if (TextUtils.isEmpty(nomeText)) {
-//            Toast toast = Toast.makeText(contexto, "O nome nao pode ser vazio", duracao);
-//            toast.show();
-//        } else if (nomeText.length() < 3) {
-//            Toast toast = Toast.makeText(contexto, "Insira um nome de tamanho valido", duracao);
-//            toast.show();
-//        } else if (TextUtils.isEmpty(emailText)) {
-//            Toast toast = Toast.makeText(contexto, "O Email nao pode ser vazio", duracao);
-//            toast.show();
-//        } else if (TextUtils.isEmpty(senhaText)) {
-//            Toast toast = Toast.makeText(contexto, "A Senha nao pode ser vazio", duracao);
-//            toast.show();
-//        } else if (TextUtils.isEmpty(confSenhaText)) {
-//            Toast toast = Toast.makeText(contexto, "A Confirmação de senha nao pode ser vazio", duracao);
-//            toast.show();
-//        } else if (letrasRegex) {
-//            Toast toast = Toast.makeText(contexto, "Entre somente letras no nome", duracao);
-//            toast.show();
-//        } else if (!emailRegex) {
-//            Toast toast = Toast.makeText(contexto, "Entre um email valido", duracao);
-//            toast.show();
-//        } else if (numerosRegex) {
-//            Toast toast = Toast.makeText(contexto, "Entre somente numeros na matricula", duracao);
-//            toast.show();
-//        } else if (!senhaRegex) {
-//            Toast toast = Toast.makeText(contexto, "Senha invalida", duracao);
-//            toast.show();
-//        } else if (!confSenhaRegex) {
-//            Toast toast = Toast.makeText(contexto, "Confirmação de senha invalida", duracao);
-//            toast.show();
-//        } else if (!senhaText.equals(confSenhaText)) {
-//            Toast toast = Toast.makeText(contexto, "A senha e a confirmação devem ser iguais", duracao);
-//            toast.show();
-//        } else if (checkCie.isChecked() && cieEscolhido) {
-//            Toast toast = Toast.makeText(contexto, "O curso escolhido não pode ser o não cursaria", duracao);
-//            toast.show();
-//        } else if (checkAds.isChecked() && adsEscolhido) {
-//            Toast toast = Toast.makeText(contexto, "O curso escolhido não pode ser o não cursaria", duracao);
-//            toast.show();
-//        } else if (checkEng.isChecked() && engEscolhido) {
-//            Toast toast = Toast.makeText(contexto, "O curso escolhido não pode ser o não cursaria", duracao);
-//            toast.show();
-//        }
-        else {
+        if (TextUtils.isEmpty(matriculaText)) {
+            Toast toast = Toast.makeText(contexto, "A matrícula não pode ser vazio", duracao);
+            toast.show();
+        } else if (matriculaText.length() > 20) {
+            Toast toast = Toast.makeText(contexto, "Insira uma matricula de tamanho valido", duracao);
+            toast.show();
+        } else if (TextUtils.isEmpty(nomeText)) {
+            Toast toast = Toast.makeText(contexto, "O nome não pode ser vazio", duracao);
+            toast.show();
+        } else if (nomeText.length() < 3) {
+            Toast toast = Toast.makeText(contexto, "Insira um nome de tamanho valido", duracao);
+            toast.show();
+        } else if (TextUtils.isEmpty(emailText)) {
+            Toast toast = Toast.makeText(contexto, "O Email não pode ser vazio", duracao);
+            toast.show();
+        } else if (TextUtils.isEmpty(senhaText)) {
+            Toast toast = Toast.makeText(contexto, "A Senha não pode ser vazio", duracao);
+            toast.show();
+        } else if (TextUtils.isEmpty(confSenhaText)) {
+            Toast toast = Toast.makeText(contexto, "A Confirmação de senha não pode ser vazio", duracao);
+            toast.show();
+        } else if (letrasRegex) {
+            Toast toast = Toast.makeText(contexto, "Entre somente letras no nome", duracao);
+            toast.show();
+        } else if (!emailRegex) {
+            Toast toast = Toast.makeText(contexto, "Entre um e-mail valido", duracao);
+            toast.show();
+        } else if (numerosRegex) {
+            Toast toast = Toast.makeText(contexto, "Entre somente números na matricula", duracao);
+            toast.show();
+        } else if (!senhaRegex) {
+            Toast toast = Toast.makeText(contexto, "Senha inválida", duracao);
+            toast.show();
+        } else if (!confSenhaRegex) {
+            Toast toast = Toast.makeText(contexto, "Confirmação de senha inválida", duracao);
+            toast.show();
+        } else if (!senhaText.equals(confSenhaText)) {
+            Toast toast = Toast.makeText(contexto, "A senha e a confirmação devem ser iguais", duracao);
+            toast.show();
+        } else if (checkCiencias.isChecked() && cieEscolhido) {
+            Toast toast = Toast.makeText(contexto, "O curso escolhido não pode ser o mesmo denão cursaria", duracao);
+            toast.show();
+        } else if (checkAds.isChecked() && adsEscolhido) {
+            Toast toast = Toast.makeText(contexto, "O curso escolhido não pode ser o mesmo de não cursaria", duracao);
+            toast.show();
+        } else if (checkEngenharia.isChecked() && engEscolhido) {
+            Toast toast = Toast.makeText(contexto, "O curso escolhido não pode ser o mesmo de não cursaria", duracao);
+            toast.show();
+        } else {
             texto = "Tudo OK";
             Toast toast = Toast.makeText(contexto, texto, duracao);
             toast.show();
